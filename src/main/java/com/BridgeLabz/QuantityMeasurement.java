e53666c101c59bd9f5e1fd4a6f02f6ac8c525eb0
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-10 11:06:44 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/BridgeLabz/QuantityMeasurement.java
@@ -0,0 +1,8 @@
+package com.BridgeLabz;
+
+public class QuantityMeasurement
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the quantity measurement");
+    }
+}
---
+++ src/test/java/com/BridgeLabz/TestCaseForQuantityMeasurement.java
@@ -0,0 +1,6 @@
+package com.BridgeLabz;
+
+public class TestCaseForQuantityMeasurement
+{
+
+}
