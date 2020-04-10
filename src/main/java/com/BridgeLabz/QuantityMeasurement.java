c0bf9f18d02e7297143cc85398020f7841e76251
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-10 20:37:34 +0530

    ADDED : Method for calculating the length and checking the given input

--- src/main/java/com/BridgeLabz/QuantityMeasurement.java
+++ src/main/java/com/BridgeLabz/QuantityMeasurement.java
@@ -2,7 +2,16 @@ package com.BridgeLabz;
 
 public class QuantityMeasurement
 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the quantity measurement");
+    public double calculateLength(int foot, double inches) {
+        double totalLength = foot*inches;
+        return totalLength;
     }
+    public boolean calLength(int foot , int inch)
+    {
+        if (foot == inch)
+            return true;
+        else
+            return false;
+    }
+
 }
