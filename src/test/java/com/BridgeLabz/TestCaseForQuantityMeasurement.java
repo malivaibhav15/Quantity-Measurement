a1da81305230b02fb396b08db66b5bd3c22c8892
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-11 13:58:27 +0530

    ADDED : Test case for checking the equality

--- src/test/java/com/BridgeLabz/TestCaseForQuantityMeasurement.java
+++ src/test/java/com/BridgeLabz/TestCaseForQuantityMeasurement.java
@@ -17,12 +17,27 @@ public class TestCaseForQuantityMeasurement
     }
 
     @Test
-    public void givenZeroFootAndZeroInche_WhenBothSame_ShouldRerturnEqual()
+    public void givenZeroFootAndZeroFoot_WhenBothSame_ShouldRerturnEqual()
     {
         int foot = 0;
-        int inches = 0;
-        boolean totalLength = quantityMeasurement.calLength(foot,inches);
+        int foot1 = 0;
+        boolean totalLength = quantityMeasurement.calLength(foot,foot1);
         Assert.assertEquals(true,totalLength);
     }
+    @Test
+    public void givenNull_ToBoth_ShouldReturnTrue()
+    {
+        boolean value = quantityMeasurement.equals(null);
+        boolean value1 = quantityMeasurement.equals(null);
+        Assert.assertEquals(value,value1);
+    }
 
+    @Test
+    public void givenValueForEquality_WhenBothSame_ThnenShouldReturnTrue()
+    {
+        int foot = 5;
+        int foot1 = 5;
+        boolean result = quantityMeasurement.calLength(foot,foot1);
+        Assert.assertEquals(true,result);
+    }
 }
