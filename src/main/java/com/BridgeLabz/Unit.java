b497237b32be57264c3072688aaaff78225b2128
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-13 00:22:34 +0530

    ADDED : Gallon and litre in the enum

--- src/main/java/com/BridgeLabz/Unit.java
+++ src/main/java/com/BridgeLabz/Unit.java
@@ -4,7 +4,10 @@ public enum Unit {
     FEET(12),
     INCH(1),
     YARD(36),
-    CENTIMETER(0.4);
+    CENTIMETER(0.4),
+    GALLON(3.78),
+    LITRE(1),
+    ML(0.001);;
 
     private final double value;
 
