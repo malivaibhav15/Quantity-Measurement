28d0161ffe75825c4e220d3a14fa02160c8af820
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-12 23:21:07 +0530

    ADDED : Exception for quantity measurement

--- src/main/java/com/BridgeLabz/QuantityMeasurementException.java
+++ src/main/java/com/BridgeLabz/QuantityMeasurementException.java
@@ -1,4 +1,14 @@
 package com.BridgeLabz;
 
-public class QuantityMeasurementException {
+public class QuantityMeasurementException extends Throwable {
+    public ExceptionType type;
+
+    public QuantityMeasurementException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
+    public enum ExceptionType {
+        NULL_POINTER_EXCEPTION
+    }
 }
