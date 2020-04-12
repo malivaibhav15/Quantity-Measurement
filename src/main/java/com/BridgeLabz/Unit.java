2cf0fb97bea51b9a718b79c7efa5345e0310758e
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-12 23:21:52 +0530

    ADDED : Enum clas for units

--- src/main/java/com/BridgeLabz/Unit.java
+++ src/main/java/com/BridgeLabz/Unit.java
@@ -1,4 +1,17 @@
 package com.BridgeLabz;
 
 public enum Unit {
+    FEET(12),
+    INCH(1),
+    YARD(36);
+
+    private final double value;
+
+    Unit(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
